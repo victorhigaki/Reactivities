832dbc53b87db3152b2f66e5e70fff616c5aabb1
victorhigaki <victorhigaki@outlook.com>
2020-02-10 20:18:16 -0300

    typescript basics

---
+++ client-app/src/demo.ts
@@ -0,0 +1,78 @@
+let data = 42;
+data = "42";
+
+let data;
+data = "42";
+
+let data: number;
+data = "42";
+
+let data: any;
+data = "42";
+
+let data: number | string;
+data = "42";
+
+let data = 42;
+data = 10;
+
+const car1 = {
+  color: "blue",
+  model: "BMW"
+};
+const car2 = {
+  color: "red",
+  model: "Mercedes",
+  topSpeed: 100
+};
+
+interface ICar {
+  color: string;
+  model: string;
+}
+const car1: ICar = {
+  color: "blue",
+  model: "BMW"
+};
+const car2: ICar = {
+  color: "red",
+  model: "Mercedes",
+  topSpeed: 100
+};
+
+interface ICar {
+  color: string;
+  model: string;
+  topSpeed?: number;
+}
+
+const car1: ICar = {
+  color: "blue",
+  model: "BMW"
+};
+
+const car2: ICar = {
+  color: "red",
+  model: "Mercedes",
+  topSpeed: 100
+};
+
+const multiply = (x: any, y: any) => {
+  return x * y;
+};
+
+const multiply = (x: number, y: number) => {
+  return x * y;
+};
+
+const multiply = (x: number, y: number): string => {
+  return (x * y).toString();
+};
+
+const multiply = (x: number, y: number): void => {
+  x * y;
+};
+
+const multiply = (x: number, y: number) => {
+  x * y;
+};
