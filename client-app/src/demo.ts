e37d1cbebdb73f9f26661f15d608461f1de6c144
victorhigaki <victorhigaki@outlook.com>
2020-02-10 21:27:38 -0300

    typescript with react demo

--- client-app/src/demo.ts
+++ client-app/src/demo.ts
@@ -1,46 +1,46 @@
-let data = 42;
-data = "42";
+// let data = 42;
+// data = "42";
 
-let data;
-data = "42";
+// let data;
+// data = "42";
 
-let data: number;
-data = "42";
+// let data: number;
+// data = "42";
 
-let data: any;
-data = "42";
+// let data: any;
+// data = "42";
 
-let data: number | string;
-data = "42";
+// let data: number | string;
+// data = "42";
 
 let data = 42;
 data = 10;
 
-const car1 = {
-  color: "blue",
-  model: "BMW"
-};
-const car2 = {
-  color: "red",
-  model: "Mercedes",
-  topSpeed: 100
-};
+// const car1 = {
+//   color: "blue",
+//   model: "BMW"
+// };
+// const car2 = {
+//   color: "red",
+//   model: "Mercedes",
+//   topSpeed: 100
+// };
 
-interface ICar {
-  color: string;
-  model: string;
-}
-const car1: ICar = {
-  color: "blue",
-  model: "BMW"
-};
-const car2: ICar = {
-  color: "red",
-  model: "Mercedes",
-  topSpeed: 100
-};
+// interface ICar {
+//   color: string;
+//   model: string;
+// }
+// const car1: ICar = {
+//   color: "blue",
+//   model: "BMW"
+// };
+// const car2: ICar = {
+//   color: "red",
+//   model: "Mercedes",
+//   topSpeed: 100
+// };
 
-interface ICar {
+export interface ICar {
   color: string;
   model: string;
   topSpeed?: number;
@@ -57,21 +57,23 @@ const car2: ICar = {
   topSpeed: 100
 };
 
-const multiply = (x: any, y: any) => {
-  return x * y;
-};
+export const cars = [car1, car2]
 
-const multiply = (x: number, y: number) => {
-  return x * y;
-};
+// const multiply = (x: any, y: any) => {
+//   return x * y;
+// };
 
-const multiply = (x: number, y: number): string => {
-  return (x * y).toString();
-};
+// const multiply = (x: number, y: number) => {
+//   return x * y;
+// };
 
-const multiply = (x: number, y: number): void => {
-  x * y;
-};
+// const multiply = (x: number, y: number): string => {
+//   return (x * y).toString();
+// };
+
+// const multiply = (x: number, y: number): void => {
+//   x * y;
+// };
 
 const multiply = (x: number, y: number) => {
   x * y;
