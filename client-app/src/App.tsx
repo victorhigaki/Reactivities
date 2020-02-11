2ad38745350348790586c823d792585e24e8c275
victorhigaki <victorhigaki@outlook.com>
2020-02-10 23:56:32 -0300

    retrieve Values from API with axios

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
   state = {
@@ -8,11 +9,11 @@ class App extends Component {
   };
 
   componentDidMount() {
-    this.setState({
-      values: [
-        { id: 1, name: 'Value 101' },
-        { id: 2, name: 'Value 102' }
-      ]
+    axios.get('http://localhost:5000/api/values').then(response => {
+      console.log(response);
+      this.setState({
+        values: response.data
+      });
     });
   }
 
@@ -23,7 +24,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <ul>
             {this.state.values.map((value: any) => (
-              <li>{value.name}</li>
+              <li key={value.id}>{value.name}</li>
             ))}
           </ul>
         </header>
