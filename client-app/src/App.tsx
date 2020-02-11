16ede8d59d60ca49cf1259650a5bc112d2255f01
victorhigaki <victorhigaki@outlook.com>
2020-02-10 21:29:40 -0300

    return changes

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,19 +1,26 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import { cars } from "./demo";
-import CarItem from "./CarItem";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-const App: React.FC = () => {
+const App = () => {
   return (
     <div className="App">
-      <ul>
-        {cars.map(car => (
-          <CarItem car={car} />
-        ))}
-      </ul>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-};
+}
 
 export default App;
