fb82c44d36a8a60b73237a3182d8aff5d1631eeb
victorhigaki <victorhigaki@outlook.com>
2020-03-08 01:06:29 -0300

    Refactor Activities List to ActivityStore

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,8 +1,24 @@
-import { observable } from 'mobx';
+import { observable, action } from 'mobx';
 import { createContext } from 'react';
+import { IActivity } from '../models/activity';
+import agent from '../api/agent';
 
 class ActivityStore {
-    @observable title = 'Hello from mobx'
+  @observable activities: IActivity[] = [];
+  @observable loadingInitial = false;
+
+  @action loadActivities = () => {
+    this.loadingInitial = true;
+
+    agent.Activities.list()
+      .then(activities => {
+        activities.forEach(activity => {
+          activity.date = activity.date.split('.')[0];
+          this.activities.push(activity);
+        });
+      })
+      .finally(() => (this.loadingInitial = false));
+  };
 }
 
-export default createContext(new ActivityStore())
+export default createContext(new ActivityStore());
