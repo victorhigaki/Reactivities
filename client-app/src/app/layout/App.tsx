45a6170041462db2dcb9b6b3c68d356b7f08a4ed
victorhigaki <victorhigaki@outlook.com>
2020-03-08 16:12:34 -0300

    Refactor Activities List to ActivityStore

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -8,10 +8,11 @@ import React, {
 import { Container } from 'semantic-ui-react';
 import { IActivity } from '../models/activity';
 import { NavBar } from '../../features/nav/NavBar';
-import { ActivityDashboard } from '../../features/activities/dashboard/ActivityDashboard';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import agent from '../api/agent';
 import { LoadingComponent } from './LoadingComponent';
 import ActivityStore from '../stores/activityStore';
+import { observer } from 'mobx-react-lite';
 
 const App = () => {
   const activityStore = useContext(ActivityStore);
@@ -73,27 +74,18 @@ const App = () => {
   };
 
   useEffect(() => {
-    agent.Activities.list()
-      .then(response => {
-        let activities: IActivity[] = [];
-        response.forEach(activity => {
-          activity.date = activity.date.split('.')[0];
-          activities.push(activity);
-        });
-        setActivities(activities);
-      })
-      .then(() => setLoading(false));
-  }, []);
+    activityStore.loadActivities();
+  }, [activityStore]);
 
-  if (loading) return <LoadingComponent content="Loading activities..." />;
+  if (activityStore.loadingInitial)
+    return <LoadingComponent content="Loading activities..." />;
 
   return (
     <Fragment>
       <NavBar openCreateForm={handleOpenCreateForm} />
       <Container style={{ marginTop: '7em' }}>
-        <h1>{activityStore.title}</h1>
         <ActivityDashboard
-          activities={activities}
+          activities={activityStore.activities}
           selectActivity={handleSelectActivty}
           selectedActivity={selectedActivity}
           editMode={editMode}
@@ -110,4 +102,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default observer(App);
--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,16 +1,20 @@
 import React, { SyntheticEvent } from 'react';
 import { Item, Button, Label, Segment } from 'semantic-ui-react';
 import { IActivity } from '../../../app/models/activity';
+import { observer } from 'mobx-react-lite';
 
 interface IProps {
   activities: IActivity[];
   selectActivity: (id: string) => void;
-  deleteActivity: (event: SyntheticEvent<HTMLButtonElement>, id: string) => void;
+  deleteActivity: (
+    event: SyntheticEvent<HTMLButtonElement>,
+    id: string
+  ) => void;
   submitting: boolean;
   target: string;
 }
 
-export const ActivityList: React.FC<IProps> = ({
+const ActivityList: React.FC<IProps> = ({
   activities,
   selectActivity,
   deleteActivity,
@@ -41,7 +45,7 @@ export const ActivityList: React.FC<IProps> = ({
                 <Button
                   name={activity.id}
                   loading={target === activity.id && submitting}
-                  onClick={(e) => deleteActivity(e, activity.id)}
+                  onClick={e => deleteActivity(e, activity.id)}
                   floated="right"
                   content="Delete"
                   color="red"
@@ -55,3 +59,5 @@ export const ActivityList: React.FC<IProps> = ({
     </Segment>
   );
 };
+
+export default observer(ActivityList);
