c55a5ee0fc9f9d20de6f59b12b3d2b326fdcabf2
victorhigaki <victorhigaki@outlook.com>
2020-03-17 22:51:56 -0300

    Using Error Handling Middleware

--- Application/Activities/Details.cs
+++ Application/Activities/Details.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
+using Application.Errors;
 using Domain;
 using MediatR;
 using Persistence;
@@ -22,10 +24,14 @@ namespace Application.Activities
                 _context = context;
             }
 
-            public async Task<Activity> Handle(Query request, 
+            public async Task<Activity> Handle(Query request,
                 CancellationToken cancellationToken)
             {
                 var activity = await _context.Activities.FindAsync(request.Id);
+
+                if (activity == null)
+                    throw new RestException(HttpStatusCode.NotFound, new { activity = "Not found" });
+
                 return activity;
             }
         }
