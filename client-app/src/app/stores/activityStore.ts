012c5ffa04edf075294313b9b3488672c340f074
Victor Higaki <higvic@gmail.com>
2020-04-23 00:36:22 -0300

    toastr error submitting data

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -3,6 +3,7 @@ import { createContext, SyntheticEvent } from 'react';
 import { IActivity } from '../models/activity';
 import agent from '../api/agent';
 import { history } from '../..';
+import { toast } from 'react-toastify';
 
 configure({ enforceActions: 'always' });
 
@@ -98,7 +99,8 @@ class ActivityStore {
       runInAction('create activity error', () => {
         this.submitting = false;
       });
-      console.log(error);
+      toast.error('Problem submitting data');
+      console.log(error.response);
     }
   };
 
@@ -116,7 +118,8 @@ class ActivityStore {
       runInAction('edit activity error', () => {
         this.submitting = false;
       });
-      console.log(error);
+      toast.error('Problem submitting data');
+      console.log(error.response);
     }
   };
 
