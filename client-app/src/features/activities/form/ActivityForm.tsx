0e44d0580e8845a76365eb1b2a1dd126f952267d
Victor Higaki <higvic@gmail.com>
2020-04-23 23:15:03 -0300

    adjusts

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,6 +1,6 @@
-import React, { useState, FormEvent, useContext, useEffect } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { Segment, Form, Button, Grid } from 'semantic-ui-react';
-import { IActivity, IActivityFormValues, ActivityFormValues } from '../../../app/models/activity';
+import { ActivityFormValues } from '../../../app/models/activity';
 import { v4 as uuid } from 'uuid';
 import ActivityStore from '../../../app/stores/activityStore';
 import { observer } from 'mobx-react-lite';
@@ -8,21 +8,31 @@ import { RouteComponentProps } from 'react-router';
 import { Form as FinalForm, Field } from 'react-final-form';
 import TextInput from '../../../app/common/form/TextInput';
 import TextAreaInput from '../../../app/common/form/TextAreaInput';
-import { category } from '../../../app/common/options/categoryOptions';
 import SelectInput from '../../../app/common/form/SelectInput';
 import DateInput from '../../../app/common/form/DateInput';
+import { category } from '../../../app/common/options/categoryOptions';
 import { combineDateAndTime } from '../../../app/common/util/util';
-import { combineValidators, isRequired, composeValidators, hasLengthGreaterThan } from "revalidate";
+import {
+  combineValidators,
+  isRequired,
+  composeValidators,
+  hasLengthGreaterThan
+} from 'revalidate';
 
 const validate = combineValidators({
   title: isRequired({ message: 'The event title is required' }),
   category: isRequired('Category'),
-  description: composeValidators(isRequired('Description'), hasLengthGreaterThan(4)({ message: 'Description needs to be at least 5 characters' }))(),
+  description: composeValidators(
+    isRequired('Description'),
+    hasLengthGreaterThan(4)({
+      message: 'Description needs to be at least 5 characters'
+    })
+  )(),
   city: isRequired('City'),
   venue: isRequired('Venue'),
   date: isRequired('Date'),
   time: isRequired('Time')
-})
+});
 
 interface DetailParams {
   id: string;
@@ -37,26 +47,22 @@ const ActivityForm: React.FC<RouteComponentProps<DetailParams>> = ({
     createActivity,
     editActivity,
     submitting,
-    activity: initialFormState,
-    loadActivity,
-    clearActivity
+    loadActivity
   } = activityStore;
 
-  const [activity, setActivity] = useState<ActivityFormValues>(new ActivityFormValues());
+  const [activity, setActivity] = useState(new ActivityFormValues());
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     if (match.params.id) {
       setLoading(true);
-      loadActivity(match.params.id).then(
-        (activity) => setActivity(new ActivityFormValues(activity))
-      ).finally(() => setLoading(false));
+      loadActivity(match.params.id)
+        .then((activity) => {
+          setActivity(new ActivityFormValues(activity));
+        })
+        .finally(() => setLoading(false));
     }
-
-  }, [
-    loadActivity,
-    match.params.id,
-  ]);
+  }, [loadActivity, match.params.id]);
 
   const handleFinalFormSubmit = (values: any) => {
     const dateAndTime = combineDateAndTime(values.date, values.time);
@@ -116,9 +122,10 @@ const ActivityForm: React.FC<RouteComponentProps<DetailParams>> = ({
                     name="time"
                     time={true}
                     placeholder="Time"
-                    value={activity.date}
+                    value={activity.time}
                   />
                 </Form.Group>
+
                 <Field
                   component={TextInput}
                   name="city"
