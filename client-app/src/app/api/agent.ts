155317616b5ddc86e2e63ebe8cea2f00c1cd8149
Victor Higaki <higvic@gmail.com>
2020-05-05 00:43:39 -0300

    user interface and axios methods

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -2,6 +2,7 @@ import axios, { AxiosResponse } from 'axios';
 import { IActivity } from '../models/activity';
 import { history } from '../..';
 import { toast } from 'react-toastify';
+import { IUser, IUserFormValues } from '../models/user';
 
 axios.defaults.baseURL = 'http://localhost:5000/api';
 
@@ -64,6 +65,12 @@ const Activities = {
     requests.put(`/activities/${activity.id}`, activity),
   delete: (id: string) => requests.del(`/activities/${id}`)
 };
+
+const User = {
+  current: (): Promise<IUser> => requests.get('/user'),
+  login: (user: IUserFormValues): Promise<IUser> => requests.post('/user/login', user),
+  register: (user: IUserFormValues): Promise<IUser> => requests.post('/user/register', user),
+}
 
 export default {
   Activities
