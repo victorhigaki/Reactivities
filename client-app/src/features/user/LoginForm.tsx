0742d1a4ab8bcac29ac977a575facbdee86611a2
Victor Higaki <higvic@gmail.com>
2020-05-07 23:44:19 -0300

    login form validation

--- client-app/src/features/user/LoginForm.tsx
+++ client-app/src/features/user/LoginForm.tsx
@@ -1,9 +1,16 @@
 import React, { useContext } from 'react';
 import { Form as FinalForm, Field } from 'react-final-form';
 import TextInput from '../../app/common/form/TextInput';
-import { Form, Button } from 'semantic-ui-react';
+import { Form, Button, Label } from 'semantic-ui-react';
 import { RootStoreContext } from '../../app/stores/rootStore';
 import { IUserFormValues } from '../../app/models/user';
+import { FORM_ERROR } from 'final-form';
+import { combineValidators, isRequired } from 'revalidate';
+
+const validate = combineValidators({
+  email: isRequired('email'),
+  password: isRequired('password')
+});
 
 const LoginForm = () => {
   const rootStore = useContext(RootStoreContext);
@@ -11,8 +18,21 @@ const LoginForm = () => {
 
   return (
     <FinalForm
-      onSubmit={(values: IUserFormValues) => login(values)}
-      render={({ handleSubmit }) => (
+      onSubmit={(values: IUserFormValues) =>
+        login(values).catch((error) => ({
+          [FORM_ERROR]: error
+        }))
+      }
+      validate={validate}
+      render={({
+        handleSubmit,
+        submitting,
+        form,
+        submitError,
+        invalid,
+        pristine,
+        dirtySinceLastSubmit
+      }) => (
         <Form onSubmit={handleSubmit}>
           <Field name="email" component={TextInput} placeholder="Email" />
           <Field
@@ -21,7 +41,17 @@ const LoginForm = () => {
             placeholder="Password"
             type="password"
           />
-          <Button positive content="Login" />
+          {submitError && !dirtySinceLastSubmit && (
+            <Label color="red" basic content={submitError.statusText} />
+          )}
+          <br />
+          <Button
+            disabled={invalid && !dirtySinceLastSubmit || pristine}
+            loading={submitting}
+            positive
+            content="Login"
+          />
+          <pre>{JSON.stringify(form.getState(), null, 2)}</pre>
         </Form>
       )}
     />
